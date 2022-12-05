25de72ef07aca4123d5fe469854c2a6ab201bc1d
eaU520 <ecallowayllc@gmail.com>
2022-12-04 22:08:44 -0500

    Capturing the input

--- shortener/src/App.vue
+++ shortener/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import URLInput from './components/URLInput.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -8,12 +8,11 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <URLInput />
   </main>
 </template>
 
---
+++ shortener/src/components/URLInput.vue
@@ -0,0 +1,40 @@
+<script>
+export default{
+    data(){
+        return {
+            URLtext: ''
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="URL_input">
+    <h3>
+      Input a URL
+        <p>The original URL is: {{ URLtext }}</p>
+        <p>The shortened URL is: {{ URLtext }}</p>
+        <input v-model="URLtext" placeholder="Put in a URL" />
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
