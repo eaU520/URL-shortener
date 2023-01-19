75c43d26b735a5fd90d7f40d3f7a7a0c60e4c306
eaU520 <ecallowayllc@gmail.com>
2023-01-18 22:08:41 -0500

    Starting to fix API call

--- shortener/src/components/URLInput.vue
+++ shortener/src/components/URLInput.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from "axios";
 export default{
     data(){
         return {
@@ -7,19 +8,53 @@ export default{
         }
     },
     methods: {
+        //Body {"long_url": "http://www.http.test.com.too_long/way_toolong"}
+        //Authorization: Bearer 346c0ceeb0de2801ee05bf54dcdbed92045095ed
+        //Content-Type: application/json
+        //Content-Length: ????
+        //Accept: application/json
+        /*
+        var fetch = require('node-fetch');
+
+fetch('https://api-ssl.bitly.com/v4/shorten', {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Bearer {TOKEN}',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Bmc53Vqc1YH" })
+});
+
+
+        */
         shortenAPICall(event){
-            alert("Test, to call API: with "+this.URLtext)
+            const token = "346c0ceeb0de2801ee05bf54dcdbed92045095ed";
+            // alert("Test, to call API: with "+this.URLtext)
+            //Axios.defaults.headers.post['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+            const stuffs = {
+                'Authorization': 'Bearer '+token,
+                'Content-Type': 'application/json'
+            };
+            try{
+             const response = await axios.post("https://api-ssl.bitly.com/v4/shorten",{
+              headers: stuffs,
+              body: JSON.stringify({"long_url": this.URLtext, "domain": "bit.ly", "group_guid":"Bmc53Vqc1YH"})
+            })}
+            catch (e){
+              console.log(e);
+            }
+            this.URLshort = JSON.stringify(response.link);
+            },
         }
-    }
-}
+      }
 </script>
 
 <template>
   <div class="URL_input">
     <h3>
       Input a URL
-        <p>The original URL is: {{ URLshort }}</p>
-        <p>The shortened URL is: {{ URLtext }}</p>
+        <p>The original URL is: {{ URLtext }}</p>
+        <p>The shortened URL is: {{ URLshort }}</p>
         <input v-model="URLtext" placeholder="Put in a URL" />
         <button @click="shortenAPICall">Shorten!</button>
     </h3>
@@ -38,10 +73,10 @@ h3 {
 }
 
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
     text-align: left;
   }
-}
+} */
 </style>
